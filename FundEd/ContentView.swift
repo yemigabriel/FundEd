639e508cbbb694d1145222625ed38a517feca1e4
Yemi Gabriel <yemigabriel@gmail.com>
2021-08-14 18:53:33 +0100

    Initial Commit

---
+++ FundEd/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FundEd
+//
+//  Created by Yemi Gabriel on 8/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FundEd/FundEdApp.swift
@@ -0,0 +1,17 @@
+//
+//  FundEdApp.swift
+//  FundEd
+//
+//  Created by Yemi Gabriel on 8/14/21.
+//
+
+import SwiftUI
+
+@main
+struct FundEdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
